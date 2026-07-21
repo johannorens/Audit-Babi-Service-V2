45fd9db147b4947478592a1d04b79b8f8da39d41
johannorens <johannorens009@gmail.com>
2026-07-21 20:15:47 +0000

    modif des routes avec throttle

--- babi/routes/api.php
+++ babi/routes/api.php
@@ -9,8 +9,10 @@ use App\Http\Controllers\Api\AdminDashboardController;
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login',    [AuthController::class, 'login']);
+Route::middleware('throttle:5,1')->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login',    [AuthController::class, 'login']);
+});
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
